59c88cdb59b7d86b6a5a5c5c4509c32b21828bf9
mina bergova <bergovam@gmail.com>
2025-07-02 18:24:04 +0300

    Setup handlebars

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ JS Back-End workshop
 ## developibg steps
  1. Initial project architecture
  2. Setup Express
+ 3. Setup handlebars
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+
 import express from 'express';
+import handlebars from 'express-handlebars'; 
 
 const app = express(); // export app
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+    res.render('index', {layout: false})
+})
 
 app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
